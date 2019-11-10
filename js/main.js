b194bb82c6086bedbc86221a695e8811ae50413e
T. Lanette <TLPollard4516@gmail.com>
2019-11-10 11:45:44 -0500

    Styling Mobile Navbar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-/* Toggle Button */
-let mobileNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function(){
-    mobileNav.classList.toggle('active');
-});
+//Navigation 
