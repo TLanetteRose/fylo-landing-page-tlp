ed42e207f49234e749c32ca19cde8d3ffcdd59ad
T. Lanette <TLPollard4516@gmail.com>
2019-11-10 14:46:58 -0500

    Header styling
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 //Navigation 
+function navFunction() {
+    let mobileNav = document.getElementById("fyloTopnav");
+    if (mobileNav.className === "topnav") {
+        mobileNav.className += " responsive";
+    } else {
+        mobileNav = "topnav";
+    }
+}
